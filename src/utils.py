50059f74841f8906cf99d43d73fe9c909fdcccf2
Mateusz Zawadzki <mtszzwdzk@gmail.com>
2018-08-28 19:46:29 +0200

    Create ImageObject representation

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,11 @@ import os
 import shutil
 import logging
 
+from PIL import Image
+
 logger = logging.getLogger(__name__)
 
+""" RENAME IT TO TEMP CLONER """
 class FileCloner:
     TMP = '/tmp'
 
@@ -20,7 +23,64 @@ class FileCloner:
             logger.error('Error while copying to temp', e)
             return False
 
+    def remove_temp_dir(self):
+        shutil.rmtree(self.temporary_path())
+
     @property
     def temporary_path(self):
         return os.path.join(self.TMP)
+
+
+
+class ImageObject:
+    def __init__(self, name, dir_path):
+        self.name = name
+        self.image_path = os.path.join(dir_path, name)
+        self.instance = self.open()
+        self.extension = None # TODO: extensions!
+
+    def open(self):
+        try:
+            return Image.open(self.image_path)
+        except Exception as e:
+            # TODO: error handling
+            pass
+
+    def get_extension(self):
+        return self.name.split('.')[-1]
+
+    def is_valid_extension(self):
+        return True
+
+
+
+    def get_exif_data(self):
+        return self.instance._getexif()
+
+    def __str__(self):
+        return self.name
+
+class ImageCollector:
+    @staticmethod
+    def collect(path):
+        for root, dirs, files in os.walk(path):
+            for f in files:
+                yield ImageObject(f, path)
+
+class FileSieve:
+    def __init__(self, out, dest, dir_name, date_range):
+        self.out_path = os.path.join(out)
+        self.dest_path = os.path.join(dest)
+        self.dir_name = dir_name
+        self.date_range = date_range
+        self.cloner = FileCloner(dir_name)
+
+
+    def group(self):
+        grouped_dir_path = os.path.join(self.dest_path, self.dir_name)
+        os.mkdir(grouped_dir_path)
+
+        for image_obj in ImageCollector.collect(self.out_path):
+            print(image_obj)
+
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,14 @@
 import os
 import shutil
 from unittest import TestCase
-from src.utils import FileCloner
+from src.utils import FileCloner, FileSieve, ImageObject
 
-class TestFileCloner(TestCase):
+class BaseTest(TestCase):
     FILES_NUMBER = 9
-    def setUp(self):
-        self.dir_name = 'TEST'
-        self.cloner = FileCloner(self.dir_name)
 
+    def setUp(self):
         """ Create test dir with files """
+        self.dir_name = 'TEST'
         self.dir_to_clone = 'TO_CLONE'
 
         os.mkdir(os.path.join('/tmp', self.dir_to_clone))
@@ -18,15 +17,50 @@ class TestFileCloner(TestCase):
             file_name = f'{_}.txt'
             os.mknod(os.path.join('/tmp', self.dir_to_clone, file_name))
 
-
     def tearDown(self):
         shutil.rmtree(os.path.join('/tmp', self.dir_to_clone))
         shutil.rmtree(os.path.join('/tmp', self.dir_name))
 
 
+class TestFileCloner(BaseTest):
+
     def test_clone(self):
+        self.cloner = FileCloner(self.dir_name)
         src = os.path.join('/tmp', self.dir_to_clone)
         dest = os.path.join('/tmp', self.dir_name)
         self.assertTrue(self.cloner.clone(src, dest))
         self.assertEqual(len(os.listdir(dest)), self.FILES_NUMBER)
+
+
+class TestFileSieve(BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.dest_name= 'COLECTED'
+        os.mkdir(os.path.join('/tmp', self.dir_name))
+        os.mkdir(os.path.join('/tmp', self.dest_name))
+
+    def tearDown(self):
+        super().tearDown()
+        shutil.rmtree(os.path.join('/tmp', self.dest_name))
+
+    def test_group(self):
+        out = os.path.join('/tmp', self.dir_to_clone)
+        dest = os.path.join('/tmp', self.dest_name)
+        sieve = FileSieve(out, dest, 'ELO', None)
+        sieve.group()
+
+class TestImageObject(TestCase):
+
+    def test_get_extension(self):
+        obj = ImageObject('Name.txt', 'TEST')
+        self.assertEqual(obj.get_extension(), 'txt')
+
+        obj = ImageObject('Name-eld.txt', 'TEST')
+        self.assertEqual(obj.get_extension(), 'txt')
+
+        obj = ImageObject('dd.Name-eld.png', 'TEST')
+        self.assertEqual(obj.get_extension(), 'png')
+
+
+
 
